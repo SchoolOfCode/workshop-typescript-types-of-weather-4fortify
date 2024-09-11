d0a45a1f54ff200acc2c5a4eb5981210c5c88945
Richard Lamb <lamby42@live.co.uk>
2024-09-11 13:54:49 +0100

    added api call from fireplace palace as POC

--- 4FortifyWeatherApp/src/components/SearchBar/SearchBar.tsx
+++ 4FortifyWeatherApp/src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,28 @@
-// import { useState } from 'react' 
-import "./SearchBar.css"
+import { useState } from "react";
+import "./SearchBar.css";
+import { useEffect } from "react";
 
 export default function SearchBar() {
-  return (
-    <>
-      <input type="text" />
-      <button>Search weather for city</button>
-      <p>TODO - RESULT GOES HERE</p>
-    </>
-  )
+    let [postCode, setPostCode] = useState("");
+    let [fetchedData, setFetchedData] = useState(null);
+
+    const handleClick = (postCode) => setPostCode(postCode);
+
+    useEffect(() => {
+        if (postCode) {
+            fetch(
+                `https://seal-app-336e8.ondigitalocean.app/reviews?country=${postCode}`
+            )
+                .then((response) => response.json())
+                .then((data) => setFetchedData(data));
+        }
+    }, [postCode]);
+
+    return (
+        <>
+            <input type="text" placeholder="Search your city!" />
+            <button onClick={() => handleClick("England")}>Go</button>
+            {fetchedData && fetchedData.text}
+        </>
+    );
 }
